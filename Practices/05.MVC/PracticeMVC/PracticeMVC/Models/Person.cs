f5f7923a988e573602e0c8d4040186b77f33bd98
jcclavijod <47200341+jcclavijod@users.noreply.github.com>
2022-03-21 19:02:47 -0500

    MVC Person

---
+++ Practices/05.MVC/PracticeMVC/PracticeMVC/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace PracticeMVC.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
+        public char Sex { get; set; }
+    }
+}
