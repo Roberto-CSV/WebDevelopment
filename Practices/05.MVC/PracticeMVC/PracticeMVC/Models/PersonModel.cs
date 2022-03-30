7cab2eab8f8832854e16f3475b3d018323611e5f
jcclavijod <47200341+jcclavijod@users.noreply.github.com>
2022-03-29 20:52:44 -0500

    inicio bootstrap

--- Practices/05.MVC/PracticeMVC/PracticeMVC/Models/PersonModel.cs
+++ Practices/05.MVC/PracticeMVC/PracticeMVC/Models/PersonModel.cs
@@ -1,13 +1,15 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PracticeMVC.Models
 {
-    public class Person
+    public class PersonModel
     {
         public int Id { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public System.DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public char Sex { get; set; }
     }
 }
