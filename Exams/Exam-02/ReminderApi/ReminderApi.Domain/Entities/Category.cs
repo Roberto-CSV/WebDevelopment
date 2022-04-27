2a471dc3686b96d7e79d7f6c9e5a8c9d7e8bd5e7
Roberto-CSV <71284804+Roberto-CSV@users.noreply.github.com>
2022-04-27 10:31:09 -0500

    Actualization Exam-02

---
+++ Exams/Exam-02/ReminderApi/ReminderApi.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using ReminderApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderApi.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ Exams/Exam-02/ReminderApi/ReminderApi.Domain/Entities/Reminder.cs
@@ -0,0 +1,32 @@
+﻿using ReminderApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderApi.Domain.Entities
+{
+    public class Reminder : EntityBase
+    {
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public string CronExpression { get; set; }
+
+        [Required]
+        public int NumberOfTimes { get; set; }
+
+        [Required]
+        public bool Enabled { get; set; }
+
+    }
+}
