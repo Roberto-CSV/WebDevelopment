447954df9803270bba453a8f18a47ee1d6bea9bc
Eskelgun <usbdaviid-098@hotmail.com>
2022-04-28 16:39:30 -0500

    Actividades full

--- Practices/05.MVC/PracticeMVC/PracticeMVC/Controllers/PersonController.cs
+++ Practices/05.MVC/PracticeMVC/PracticeMVC/Controllers/PersonController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PracticeMVC.Models;
+using PracticeMVC.Services;
 using System;
 using System.Collections.Generic;
 
@@ -8,39 +9,36 @@ namespace PracticeMVC.Controllers
 {
     public class PersonController : Controller
     {
-        private static List<PersonModel> personList;
+        private readonly IPersonService _personService; 
 
-        public PersonController()
+        public PersonController(IPersonService personService)
         {
+            _personService = personService;
+
+
             // Mock User List
-            if (personList is null)
-            {
-                personList = new List<PersonModel>()
-            {
-               new PersonModel{Id=1, FirstName="Julio", LastName="Robles", DateOfBirth= DateTime.Parse("1974/10/08"), Sex='M'},
-               new PersonModel{Id=1, FirstName="Juan", LastName="Robles", DateOfBirth= DateTime.Parse("1996/07/27"), Sex='M'},
-               new PersonModel{Id=1, FirstName="Pilar", LastName="Lopez", DateOfBirth= DateTime.Parse("1976/10/04"), Sex='F'}
-            };
-            }
+            //if (personList is null)
+            //{
+            //    personList = new List<PersonModel>()
+            //{
+            //   new PersonModel{Id=1, FirstName="Julio", LastName="Robles", DateOfBirth= DateTime.Parse("1974/10/08"), Sex='M'},
+            //   new PersonModel{Id=1, FirstName="Juan", LastName="Robles", DateOfBirth= DateTime.Parse("1996/07/27"), Sex='M'},
+            //   new PersonModel{Id=1, FirstName="Pilar", LastName="Lopez", DateOfBirth= DateTime.Parse("1976/10/04"), Sex='F'}
+            //};
+            //}
         }
 
         // GET: PersonController
         public ActionResult Index()
         {
-            return View(personList);
+            return View(_personService.GetAll());
         }
 
         // GET: PersonController/Details/5
         public ActionResult Details(int id)
         {
-            PersonModel person = new PersonModel()
-            {
-                Id = 1,
-                FirstName = "John",
-                LastName = "Doe",
-                DateOfBirth = DateTime.Parse("1974/10/08"),
-                Sex = 'M'
-            };
+            PersonModel person = _personService.GetById(id);
+
 
             return View(person);
         }
@@ -54,10 +52,11 @@ namespace PracticeMVC.Controllers
         // POST: PersonController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(PersonModel person)
         {
             try
             {
+                _personService.Add(person);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -75,10 +74,11 @@ namespace PracticeMVC.Controllers
         // POST: PersonController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(PersonModel person)
         {
             try
             {
+                _personService.Update(person);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -100,10 +100,12 @@ namespace PracticeMVC.Controllers
         {
             try
             {
+                _personService.Delete(id);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
+
                 return View();
             }
         }
--- Practices/05.MVC/PracticeMVC/PracticeMVC/Startup.cs
+++ Practices/05.MVC/PracticeMVC/PracticeMVC/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PracticeMVC.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,7 @@ namespace PracticeMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddScoped<IPersonService, PersonService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
